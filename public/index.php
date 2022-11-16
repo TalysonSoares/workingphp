f23685b2f633e467b93852ad906a7891cb2b0c0c
TalysonSoares <talysonsoares40@gmail.com>
2022-11-16 09:30:12 -0300

    exportando as funções

--- public/index.php
+++ public/index.php
@@ -2,20 +2,6 @@
 
 $rota = $_SERVER['REQUEST_URI'];
 
-function inicio () 
-{
-    include '../src/views/inicio.phtml';
-}
-
-function listar () 
-{
-    include '../src/views/listar.phtml';
-}
-function novo () 
-{
-    include '../src/views/novo.phtml';
-}
-
 $paginas = [
     '/' => 'inicio',
     '/listar' => 'listar',
---
+++ src/controller/alunosController.php
@@ -0,0 +1,15 @@
+<?php
+
+function inicio () 
+{
+    include '../src/views/inicio.phtml';
+}
+
+function listar () 
+{
+    include '../src/views/listar.phtml';
+}
+function novo () 
+{
+    include '../src/views/novo.phtml';
+}
